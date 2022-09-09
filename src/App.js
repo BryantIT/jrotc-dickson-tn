9e3dab6a460f22dc4bd9c92801d5fb31887f27ae
Bryant Richards <dragonseyeimages@gmail.com>
2022-09-09 07:46:58 -0500

    fresh start

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 // Components
-import Nav from './components/nav/Nav'
 // Pages
 import Home from './pages/Home'
 import { Routes, Route } from "react-router-dom";
@@ -9,7 +8,6 @@ import './App.css'
 function App() {
   return (
     <div className="app">
-    <Nav />
       <Routes>
       <Route path="/" element={<Home />}></Route>
     </Routes>
